f3d265e7f5b2643b395e6ee1afcc4455ef1f70a6
Miwa Robbins <miwa@Miwas-MacBook-Air.local>
2026-02-20 08:22:38 -0500

    update website

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,4 +1,4 @@
-const bookingUrl = "https://flow-state-healing-and-massage.square.site/book";
+const bookingUrl = "https://flow-state-healing-and-massage.square.site";
 
 document.querySelectorAll("[data-booking-link]").forEach((link) => {
   link.setAttribute("href", bookingUrl);
